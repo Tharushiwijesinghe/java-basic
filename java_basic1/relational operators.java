8685f0bf8ecfb415ebc17624975564b4ff8a82a0
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-09 08:44:14 +0530

    Create relational operators.java

---
+++ java_basic1/relational operators.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int x =6;
+        int y =5;
+
+        boolean result1 = x > y;
+        System.out.println("result = " + result1);
+
+        boolean result2 = x < y;
+        System.out.println("result = " + result2);
+
+        boolean result3 = x >= y;
+        System.out.println("result = " + result3);
+
+        boolean result4 = x <= y;
+        System.out.println("result = " + result4);
+
+        boolean result5 = x != y;
+        System.out.println("result = " + result5);
+
+        boolean result6 = x == y;
+        System.out.println("result = " + result6);
+    }
+}
