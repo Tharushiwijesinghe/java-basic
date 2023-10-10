801a17307006d465cbc7bb0c73723145e4acc777
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-10 06:04:06 +0530

    Create if else.java

---
+++ java_basic1/if else.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int x = 28;
+        //whenever we have one statement under if or else don't need curley brackets
+        if(x>10 && x<=20)
+            System.out.println("Hello");
+        else
+            System.out.println("Bye");
+
+    }
+}
