1a6d69bc0122781d078edd85d4231a599ee874d3
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-13 10:18:15 +0530

    Create whileloop.java

---
+++ java_basic1/whileloop.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+
+//      while(condition)
+        while (i <= 4) {
+            System.out.println("Hi"+" " + i);
+            i++; //increment or decrement
+        }
+        System.out.println("Bye"+" " + i);
+    }
+}
