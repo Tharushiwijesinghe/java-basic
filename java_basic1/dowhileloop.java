4a0271313e0bc476e6050b990be75f14d0c121dd
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-13 12:20:43 +0530

    Create dowhileloop.java

---
+++ java_basic1/dowhileloop.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 15;
+        do {
+            System.out.println("x:"+ i);
+            i++;
+            System.out.println( "\n");
+        }while(i<20);
+    }
+}
