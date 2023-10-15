a860bb4ffd9e8d5c9178a30cccfddb305b4488b4
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-15 22:34:42 +0530

    Create no_parameters-with_return_type.java

---
+++ java_basic1/methods/no_parameters-with_return_type.java
@@ -0,0 +1,18 @@
+public class Method2 {
+    public static int sum()
+    {
+        int a = 2;
+        int b = 9;
+        int c = a+b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+//        Method2 m2obj = new Method2();
+        int res = sum();
+        res = res *5;
+        System.out.println(res);
+
+    }
+}
+//in the no parameter with return type- we use this method another output from this code output. we don't this output.we get another output from code output. 
