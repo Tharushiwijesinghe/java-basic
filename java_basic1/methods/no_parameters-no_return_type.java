2505ba96a392caba8e9b7f0c43dacf9e1c0826c5
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-15 22:12:26 +0530

    Create no_parameters-no_return_type.java

---
+++ java_basic1/methods/no_parameters-no_return_type.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void sum()
+    {
+        int a = 1;
+        int b = 3;
+         int c = a + b;
+        System.out.println(c);
+    }
+    public static void main(String[]args){
+       sum();
+    }
+}
+
+
