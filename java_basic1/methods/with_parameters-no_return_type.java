5873a302fcbb41dd50d3e147ab51ff41d2225c04
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-15 22:56:04 +0530

    Create with_parameters-no_return_type.java

---
+++ java_basic1/methods/with_parameters-no_return_type.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void method3(int a, int b)
+    {
+        int c = a + b;
+        System.out.println(c);
+    }
+    public static void main(String[] args) {
+        method3(5,7);
+
+    }
+}
