20cd5391859c6e1f3357cde55cb27c066276cbfd
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-15 23:02:33 +0530

    Create with_parameters-with_return_type.java

---
+++ java_basic1/methods/with_parameters-with_return_type.java
@@ -0,0 +1,12 @@
+public class Method4 {
+    public static int para4(int a, int b)
+    {
+        int c =  a + b;
+        return c;
+    }
+    public static void main(String[] args) {
+        int res = para4(5,6);
+        System.out.println(res);
+
+    }
+}
