f47189793614635f503ed2eff792804bc98cb3e9
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-17 07:20:20 +0530

    Create variables.java

---
+++ java_basic1/variables.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      //datatype variable
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println("Result: "+ result);
+    }
+}
