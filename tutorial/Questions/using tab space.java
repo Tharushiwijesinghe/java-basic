b703935fe65ffa3c177bff90caa3c55c875e84ef
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-17 08:15:50 +0530

    Create using tab space.java

---
+++ tutorial/Questions/using tab space.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Shopping List:");
+        System.out.println("\t"+"\t"+"Apple");
+        System.out.println("\t"+"\t"+"Banana");
+        System.out.println("\t"+"\t"+"Low-fat Milk");
+    }
+}
