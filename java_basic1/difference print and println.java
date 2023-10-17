97ce1911b9eae40c693f77938f7571a18ee2947b
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-17 11:38:55 +0530

    Create difference print and println.java

---
+++ java_basic1/difference print and println.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=29;
+        int b=45;
+        int res = a*b;
+        System.out.print("Result: "+res);
+        System.out.println(" Result: "+res);
+        
+        System.out.println(" Result: "+res);
+        System.out.print("Result: "+res);
+    }
+}
