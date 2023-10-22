c5066fe980dabda632950bc30d35f39cfa184e56
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-22 16:26:29 +0530

    Create String_class.java

---
+++ java_basic1/String_class.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String country;
+        country = "Sri Lanka";
+        //substring
+        System.out.println(country.substring(0,3));
+        //length
+        System.out.println(country.length());
+        //indexof
+        System.out.println(country.indexOf("Lanka"));
+    }
+}
