0e247ccab8fd32e9508986c8292fac30c13a90a2
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-22 18:12:57 +0530

    Create Get output of this programme.java

---
+++ tutorial/Questions/Get output of this programme.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String java;
+        String country;
+        country = "Sri Lanka";
+        java = "I love Java and Java loves me.";
+        System.out.println(java.indexOf("love"));
+        System.out.println(country.indexOf("Lanka"));
+
+    }
+}
