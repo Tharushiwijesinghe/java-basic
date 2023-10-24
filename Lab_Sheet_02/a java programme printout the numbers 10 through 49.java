349cb76515b8b611d6890b0c8db09c601560ac96
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-24 13:27:55 +0530

    Create a java programme printout the numbers 10 through 49.java

---
+++ Lab_Sheet_02/a java programme printout the numbers 10 through 49.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int arr=10;
+        for(int i=0; i<4; i++)
+        {
+            for(int j=0; j<10; j++)
+            {
+                System.out.print(arr + "\t");
+                arr++;
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
