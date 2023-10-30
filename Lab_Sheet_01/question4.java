0b41ef84a9f95879282423b7bcc329e788ed4158
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-30 20:56:18 +0530

    Create question4.java

---
+++ Lab_Sheet_01/question4.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+        JFrame myWindow;
+            myWindow =  new JFrame();
+            myWindow.setSize(300, 200);
+            myWindow.setTitle("My Window");
+            myWindow.setVisible(true);
+            myWindow.setLocation(50,50);
+
+
+
+    }
+}
