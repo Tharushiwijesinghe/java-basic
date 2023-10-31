1d944d89802ab551b61ec895b712353acecf70f0
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-31 19:53:49 +0530

    Create Book.java

---
+++ java_basic1/encapsulation/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    private String bookName;
+    private int pageNo;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+
+}
