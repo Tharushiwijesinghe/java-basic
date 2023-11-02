da20a731c4b90a40880dd0fa830ba9b576cdff0f
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-02 12:28:07 +0530

    Create print_VS_println.java

---
+++ java_basic1/print_VS_println.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //print output new line
+        System.out.println("Hello world!");
+        //print output same line
+        System.out.print("Hello world!");
+    }
+}
