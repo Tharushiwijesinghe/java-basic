ab686929b076c517b165a5391dcb071c4f14c51a
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-02 20:21:30 +0530

    Create if_condition.java

---
+++ java_basic1/if_condition.java
@@ -0,0 +1,15 @@
+import  java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter test score:");
+
+        int testScore = scanner.nextInt();
+        if(testScore < 70){
+            System.out.println("You did not pass");
+        }
+        else {
+            System.out.println("You did pass");
+        }
+    }
+}
