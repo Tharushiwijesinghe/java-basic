b249158f1f21651cfa66b921a2ea722c8ac67435
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-04 11:06:54 +0530

    Create while_statement(count_controlled_loop).java

---
+++ java_basic1/while_statement(count_controlled_loop).java
@@ -0,0 +1,13 @@
+//a count-controlled loop
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0, number = 1;
+        while (number <= 100)
+        {
+            sum = sum + number;
+            System.out.println(number);
+            number++;
+        }
+
+    }
+}
