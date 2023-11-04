535d094bb7da7692324896fa2572277e2940e34b
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-04 11:48:27 +0530

    Create while_infite_loop.java

---
+++ java_basic1/while_infite_loop.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int product = 0;
+        while (product < 500000) {
+            product = product * 5;
+            System.out.println(product);
+        }
+    }
+}
