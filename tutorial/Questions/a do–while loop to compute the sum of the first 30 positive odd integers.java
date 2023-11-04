83d95237026ed010df82502a41c9089c2708b0a2
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-04 13:07:17 +0530

    Create  a do–while loop to compute the sum of the first 30 positive odd integers.java

---
+++ tutorial/Questions/a do–while loop to compute the sum of the first 30 positive odd integers.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 0;
+        int sum = 0;
+        do{
+            sum += ((2*num)+1);
+            System.out.println(sum);
+            num++;
+        }
+        while(num < 30);
+
+    }
+}
