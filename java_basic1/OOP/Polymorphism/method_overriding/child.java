2b24de34aa601009817fd975a7ecc0857ce0a7bb
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-11-09 08:52:36 +0530

    Create child.java

---
+++ java_basic1/OOP/Polymorphism/method_overriding/child.java
@@ -0,0 +1,11 @@
+public class Child extends Parent{
+
+    public void Phone() {
+       System.out.println("nokia");
+  }
+    public static void main(String[] args)
+    {
+        Child objc = new Child();
+        objc.Phone();
+    }
+}
